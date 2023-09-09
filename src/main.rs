4a8796bbde22e3378d926e9b51740273876fb949
vkavsek <vkavsek@gmail.com>
2023-09-09 18:51:47 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#![allow(unused)]
+
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().merge(routes_hello());
+
+    //          ---> START SERVER
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("-->> LISTENING on {}\n", addr);
+
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+    //          <--- START SERVER
+}
+
+// Create routes
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handle_hello))
+        .route("/hello2/:name", get(handle_hello2))
+}
+
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+/// Handles queries and greets the user or defaults to 'World!'
+/// e.g.:    '/hello?name=Luka'
+async fn handle_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handle_hello - {:?}", "HANDLER", params);
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <i>{}!!</i>", name))
+}
+
+
+/// Handles paths and greets the user or defaults to 'World!'
+/// e.g.:    '/hello2/Luka'
+async fn handle_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handle_hello - {:?}", "HANDLER", name);
+    Html(format!("Hello <i>{}!!</i>", name))
+}
---
+++ tests/quick_dev.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+
+use anyhow::{Result};
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/hello?name=Vid").await?.print().await?;
+    hc.do_get("/hello2/Luka").await?.print().await?;
+
+    Ok(())
+}
