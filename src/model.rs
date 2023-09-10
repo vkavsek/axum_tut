4c276f13709c738f1c7defdc063b6cf05d3dc149
vkavsek <vkavsek@gmail.com>
2023-09-10 14:45:30 +0200

    More work on RESTapi model

--- src/model.rs
+++ src/model.rs
@@ -6,15 +6,61 @@ use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
 // ————>    TICKET TYPES
+
+/// Gets sent to the client so it needs to be serializable.
 #[derive(Clone, Debug, Serialize)]
 pub struct Ticket {
-    pub id: u64, 
-    pub title: String, 
+    pub id: u64,
+    pub title: String,
+}
+impl Ticket {
+    pub fn from(id: u64, title: String) -> Self {
+        Self { id, title }
+    }
 }
 
-pub struct TicketForCreate{
-
+/// Payload that is sent for Create API.
+#[derive(Deserialize)]
+pub struct TicketForCreate {
+    pub title: String,
 }
+// <————    TICKET TYPES
 
-// <————    TICKET TYPES 
+/// Shouldn't be used in production as the Vec grows infinitely.
+#[derive(Clone)]
+pub struct ModelController {
+    tickets_store: Arc<Mutex<Vec<Option<Ticket>>>>,
+}
+impl ModelController {
+    pub async fn new() -> Result<Self> {
+        Ok(Self {
+            tickets_store: Arc::default(),
+        })
+    }
 
+    /// CRUD Implementation
+    pub async fn create_ticket(&self, ticket_fc: TicketForCreate) -> Result<Ticket> {
+        let mut store = self.tickets_store.lock().unwrap();
+
+        let id = store.len() as u64;
+
+        // You could check if the title is empty and return an Error
+        let ticket = Ticket::from(id, ticket_fc.title);
+
+        store.push(Some(ticket.clone()));
+
+        Ok(ticket)
+    }
+
+    pub async fn list_tickets(&self) -> Result<Vec<Ticket>> {
+        let store = self.tickets_store.lock().unwrap();
+
+        let tickets = store.iter().filter_map(|t| t.clone()).collect();
+
+        Ok(tickets)
+    }
+
+    pub async fn delete_ticket(&self, ticket_id: u64) -> Result<Ticket> {
+        todo!()
+    }
+}
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,20 +1,21 @@
 #![allow(unused)]
 
-use anyhow::{Result};
+use anyhow::Result;
 use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    // Sends requests to the server and prints the result 
+    // Sends requests to the server and prints the result
     hc.do_get("/hello2/Luka San").await?.print().await?;
 
-    let req_login = hc.do_post("/api/login", 
+    let req_login = hc.do_post(
+        "/api/login",
         json!({
-            "uname": "demo1", 
+            "uname": "demo1",
             "pass": "1234"
-        })
+        }),
     );
 
     req_login.await?.print().await?;
