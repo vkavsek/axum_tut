948300975baa0102e0941665fb8a3b021bc798b6
vkavsek <vkavsek@gmail.com>
2023-09-11 14:27:31 +0200

    .

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -31,8 +31,8 @@ async fn quick_dev() -> Result<()> {
 
     req_login.await?.print().await?;
 
-    hc.do_get("/api/tickets").await?.print().await?;
     create_ticket.await?.print().await?;
+    create_fail_ticket.await?.print().await?;
     //delete_ticket.await?.print().await?;
     hc.do_get("/api/tickets").await?.print().await?;
 
