57f5355e6f61524e7972cb124cbc64b4fea17449
vkavsek <vkavsek@gmail.com>
2023-09-12 13:34:55 +0200

    more documentation

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -8,6 +8,9 @@ use crate::Result;
 /// Requires the client to have the correct authentication cookies in order to allow certain actions.
 /// It achieves that by using the Ctx extractor. 
 /// Check out Ctx documentation for more details.
+/// Because we expand the errors here, we can simply use Ctx (not Result<Ctx>) in all the handlers
+/// that run after the authentication.
+/// in this case all the handlers inside of '/api/' path.
 pub async fn mw_require_auth<B>(
     ctx: Result<Ctx>,
     req: Request<B>,
