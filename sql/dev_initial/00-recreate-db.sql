48c38a7df8a1582a5c74df41b53ac1d5f8146357
vkavsek <vkavsek@gmail.com>
2024-02-20 16:11:04 +0100

    testing databases

--- sql/dev_initial/00-recreate-db.sql
+++ sql/dev_initial/00-recreate-db.sql
@@ -1,4 +1,12 @@
 -- Dev ONLY — Brute Force DROP DB (for local dev and unit test)
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE
-  usename = 'app_user' OR datname='app_db';
+SELECT
+  pg_terminate_backend(pid)
+FROM
+  pg_stat_activity
+WHERE
+  usename = 'app_user'
+  OR datname = 'app_db';
 DROP DATABASE IF EXISTS app_db;
+
+CREATE USER app_user PASSWORD 'dev_only_pwd';
+CREATE DATABASE app_db owner app_user ENCODING = 'UTF-8';
