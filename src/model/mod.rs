5edd1bf5f3d28839678439076f4c0b4061a8de43
vkavsek <vkavsek@gmail.com>
2024-02-20 19:27:35 +0100

    started implementing a proper model layer

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,7 +1,22 @@
 //! Simplistic Model Layer
 //! (with mock-store layer)
-
+//!
+//! Model Layer
+//!
+//! Design:
+//!
+//! — The Model layer normalizes the application's data type structures and access.
+//! — All application code data access must go through the Model Layer.
+//! — The 'ModelManager' hold the internal states/resources needed by ModelControllers to access
+//! data. (e.g., db_pool, S3 client, redis client).
+//! — Model Controllers (e.g., 'TaskBmc', 'ProjectBmc') implement CRUD and other data access
+//! methods on a given "entity" (e.g., 'Task', 'Project').
+//! ('Bmc' is short for Backend Model Controller)
+//! — In frameworks like Axum, ModelManagers are typically used as App State.
+//! — ModelManager is designed to be passed as an argument to all Model Controller functions.
+//!
 #![allow(unused)]
+
 use crate::ctx::Ctx;
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
@@ -10,6 +25,16 @@ pub mod error;
 
 pub use self::error::{Error, Result};
 
+pub struct ModelMan {
+    // db: Db,
+}
+
+impl ModelMan {
+    pub async fn new() -> Result<Self> {
+        // FIXME: TBC
+        Ok(ModelMan {})
+    }
+}
 // ————>    TICKET TYPES
 /// Gets sent to the client so it needs to be serializable.
 #[derive(Clone, Debug, Serialize)]
