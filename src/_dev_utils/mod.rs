b1053250e9472d71868cf865d03362172aea0f6a
vkavsek <vkavsek@gmail.com>
2024-02-22 13:30:06 +0100

    added a CREATE task and unit tests for it

--- src/_dev_utils/mod.rs
+++ src/_dev_utils/mod.rs
@@ -3,6 +3,8 @@ mod dev_db;
 use tokio::sync::OnceCell;
 use tracing::debug;
 
+use crate::model::ModelManager;
+
 /// Initiealize enviroment for local development.
 /// (for early development, will be called  from main()).
 pub async fn init_dev() {
@@ -13,4 +15,16 @@ pub async fn init_dev() {
         dev_db::init_dev_db().await.unwrap();
     })
     .await;
+}
+
+pub async fn init_test() -> ModelManager {
+    static INSTANCE: OnceCell<ModelManager> = OnceCell::const_new();
+    let mm = INSTANCE
+        .get_or_init(|| async {
+            init_dev().await;
+            ModelManager::init().await.unwrap()
+        })
+        .await;
+
+    mm.clone()
 }
--- src/model/task.rs
+++ src/model/task.rs
@@ -29,7 +29,7 @@ impl TaskBmc {
     pub async fn create(_ctx: &Ctx, mm: &ModelManager, task_c: TaskForCreate) -> Result<i64> {
         let db = mm.db();
 
-        let res = sqlx::query_as::<_, (i64,)>(
+        let (id,) = sqlx::query_as::<_, (i64,)>(
             r#"
             INSERT INTO task (title) values ($1) returning id
             "#,
@@ -38,6 +38,46 @@ impl TaskBmc {
         .fetch_one(db)
         .await?;
 
-        todo!()
+        Ok(id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+    use crate::_dev_utils;
+
+    use super::*;
+    use anyhow::Result;
+
+    #[tokio::test]
+    async fn test_create_ok() -> Result<()> {
+        let mm = _dev_utils::init_test().await;
+        let ctx = Ctx::root_ctx();
+        let fx_title = "test_create_ok title";
+
+        let task_c = TaskForCreate {
+            title: fx_title.to_string(),
+        };
+        let id = TaskBmc::create(&ctx, &mm, task_c).await?;
+
+        let (title,): (String,) = sqlx::query_as(
+            r#" 
+            SELECT title FROM task WHERE id = $1
+            "#,
+        )
+        .bind(id)
+        .fetch_one(mm.db())
+        .await?;
+        assert_eq!(title, fx_title);
+
+        let count = sqlx::query(r#"DELETE FROM task WHERE id = $1"#)
+            .bind(id)
+            .execute(mm.db())
+            .await?
+            .rows_affected();
+        assert_eq!(count, 1, "Did not delete 1 row?");
+
+        Ok(())
     }
 }
