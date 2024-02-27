4229394defe909cf9a457510e2d750c6aafe8198
vkavsek <vkavsek@gmail.com>
2024-02-27 17:59:31 +0100

    added crypt to config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use crate::{Error, Result};
 use core::panic;
-use std::{env, sync::OnceLock};
+use std::{env, str::FromStr, sync::OnceLock};
 
 /// Tries to create a config from enviroment variables declared in .cargo/config.toml file.
 pub fn config() -> &'static Config {
@@ -13,6 +13,11 @@ pub fn config() -> &'static Config {
 
 #[allow(non_snake_case)]
 pub struct Config {
+    // Crypt
+    pub PWD_KEY: Vec<u8>,
+    pub TOKEN_KEY: Vec<u8>,
+
+    pub TOKEN_DURATION_SEC: f64,
     // Db
     pub DB_URL: String,
     // Web
@@ -22,6 +27,9 @@ pub struct Config {
 impl Config {
     fn load_from_env() -> Result<Config> {
         Ok(Config {
+            PWD_KEY: get_env_b64u_as_u8s("SERVICE_PWD_KEY")?,
+            TOKEN_KEY: get_env_b64u_as_u8s("SERVICE_TOKEN_KEY")?,
+            TOKEN_DURATION_SEC: get_env_parse("SERVICE_TOKEN_DURATION_SEC")?,
             DB_URL: get_env("SERVICE_DB_URL")?,
             WEB_FOLDER: get_env("SERVICE_WEB_FOLDER")?,
         })
@@ -30,4 +38,13 @@ impl Config {
 
 fn get_env(name: &'static str) -> Result<String> {
     env::var(name).map_err(|_| Error::ConfigMissingEnv(name))
+}
+
+fn get_env_parse<T: FromStr>(name: &'static str) -> Result<T> {
+    let res = get_env(name)?;
+    res.parse().map_err(|_| Error::ConfigWrongFormat(name))
+}
+
+fn get_env_b64u_as_u8s(name: &'static str) -> Result<Vec<u8>> {
+    base64_url::decode(&get_env(name)?).map_err(|_| Error::ConfigWrongFormat(name))
 }
