217c29612d7fa19384608c55c7f80ffaa6271462
vkavsek <vkavsek@gmail.com>
2024-03-04 14:23:55 +0100

    quick_dev for task_rpc

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -37,7 +37,40 @@ async fn main() -> Result<()> {
     let req_list_tasks = hc.do_post(
         "/api/rpc",
         json!({
+            "id": 2,
+            "method": "list_tasks",
+        }),
+    );
+
+    let req_update_task = hc.do_post(
+        "/api/rpc",
+        json!({
             "id": 3,
+            "method": "update_task",
+            "params": {
+                "id": 1000,
+                "data": {
+                    "title": "task AAA updated"
+                }
+            }
+        }),
+    );
+
+    let req_delete_task = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 4,
+            "method": "delete_task",
+            "params": {
+                "id": 1000,
+            }
+        }),
+    );
+
+    let req_list_tasks_2 = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 5,
             "method": "list_tasks",
         }),
     );
@@ -45,6 +78,9 @@ async fn main() -> Result<()> {
     req_login.await?.print().await?;
     req_create_task.await?.print().await?;
     req_list_tasks.await?.print().await?;
+    req_update_task.await?.print().await?;
+    req_delete_task.await?.print().await?;
+    req_list_tasks_2.await?.print().await?;
     req_logoff.await?.print().await?;
 
     Ok(())
