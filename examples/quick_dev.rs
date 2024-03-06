35f5ac4b99130bb9545d023f9b7478d1c7ef6469
vkavsek <vkavsek@gmail.com>
2024-03-06 15:31:30 +0100

    quick_dev updated

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use anyhow::Result;
 use serde_json::json;
 
@@ -14,6 +12,8 @@ async fn main() -> Result<()> {
             "pwd": "welcome"
         }),
     );
+    req_login.await?.print().await?;
+
     let req_logoff = hc.do_post(
         "/api/logoff",
         json!({
@@ -21,34 +21,31 @@ async fn main() -> Result<()> {
         }),
     );
 
-    let req_create_task = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": 1,
-            "method": "create_task",
-            "params": {
-                "data": {
-                    "title": "task AAA"
+    let mut task_ids: Vec<i64> = Vec::new();
+    for i in 0..5 {
+        let req_create_task = hc.do_post(
+            "/api/rpc",
+            json!({
+                "id": 1,
+                "method": "create_task",
+                "params": {
+                    "data": {
+                        "title": format!{"task AAA {i}"}
+                    }
                 }
-            }
-        }),
-    );
-
-    let req_list_tasks = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": 2,
-            "method": "list_tasks",
-        }),
-    );
+            }),
+        );
+        let result = req_create_task.await?;
+        task_ids.push(result.json_value::<i64>("/result/id")?);
+    }
 
     let req_update_task = hc.do_post(
         "/api/rpc",
         json!({
-            "id": 3,
+            "id": 2,
             "method": "update_task",
             "params": {
-                "id": 1000,
+                "id": task_ids[0],
                 "data": {
                     "title": "task AAA updated"
                 }
@@ -59,28 +56,36 @@ async fn main() -> Result<()> {
     let req_delete_task = hc.do_post(
         "/api/rpc",
         json!({
-            "id": 4,
+            "id": 3,
             "method": "delete_task",
             "params": {
-                "id": 1001,
+                "id": task_ids[1],
             }
         }),
     );
 
-    let req_list_tasks_2 = hc.do_post(
+    let req_list_tasks = hc.do_post(
         "/api/rpc",
         json!({
-            "id": 5,
+            "id": 4,
             "method": "list_tasks",
+            "params": {
+                    "filters": [{
+                    "title": {"$endsWith": "BB"},
+                    "done": false,
+                    },{
+                    "id": {"$in": [task_ids[2], task_ids[3]]}
+                    }],
+                    "list_options": {
+                    "order_bys": "!id"
+                    }
+            }
         }),
     );
 
-    req_login.await?.print().await?;
-    req_create_task.await?.print().await?;
-    req_list_tasks.await?.print().await?;
     req_update_task.await?.print().await?;
     req_delete_task.await?.print().await?;
-    req_list_tasks_2.await?.print().await?;
+    req_list_tasks.await?.print().await?;
     req_logoff.await?.print().await?;
 
     Ok(())
